6e1c91ed404bd36b0cfe56969f4f953932fad818
Jitesh710 <73105878+Jitesh710@users.noreply.github.com>
2022-09-05 15:19:33 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,47 @@
+//for mouse clicksS
+for (let i=0; i<document.querySelectorAll(".drum").length; i++){
+    document.querySelectorAll(".drum")[i].addEventListener("click", function() {
+        let buttonHTML = this.innerHTML;
+        makeSound(buttonHTML);
+    });
+}
+//for key presses
+document.addEventListener("keydown", function(event){
+    makeSound(event.key);
+})
+
+//to generate sounds
+function makeSound(key){
+    switch (key) {
+        case "w":
+            let tom1 = new Audio("sounds/tom-1.mp3");
+            tom1.play();
+            break;
+        case "a":
+            let tom2 = new Audio("sounds/tom-2.mp3");
+            tom2.play();
+            break;
+        case "s":
+            let tom3 = new Audio("sounds/tom-3.mp3");
+            tom3.play();
+            break;
+        case "d":
+            let tom4 = new Audio("sounds/tom-4.mp3");
+            tom4.play();
+            break;
+        case "j":
+            let crash = new Audio("sounds/crash.mp3");
+            crash.play();
+            break;
+        case "k":
+            let kick = new Audio("sounds/kick-bass.mp3");
+            kick.play();
+            break;
+            case "l":
+            let snare = new Audio("sounds/snare.mp3");
+            snare.play();
+            break;
+        default:
+            break;
+    }
+}
